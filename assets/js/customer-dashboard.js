2d05cf937f33e4c5dfe3572f195eddb68be66016
Harish-carmatec <harish@carmatec.in>
2018-04-03 22:11:14 +0530

    test

---
+++ assets/js/customer-dashboard.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  
+});
+$(function () {
+    $('.navbar-toggle').click(function () {
+        $('.navbar-nav').toggleClass('slide-in');
+        $('.side-body').toggleClass('body-slide-in');
+
+        /// uncomment code for absolute positioning tweek see top comment in css
+        //$('.absolute-wrapper').toggleClass('slide-in');
+        
+    });
+});
